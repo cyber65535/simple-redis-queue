aeba0a7c43c9449f48b015ecd49b28eee4a7e319
FrankJiao <frank@dailyinnovation.biz>
2018-08-10 20:26:55 +0800

    init

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import os
+import os.path import join
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+base_dir = os.path.dirname(__file__)
+readme_path = join(base_dir, 'README.md')
+changes = join(base_dir, "CHANGES")
+
+__pkginfo__ = {}
+with open(os.path.join(base_dir, "src", "__pkginfo__.py")) as f:
+    exec(f.read(), __pkginfo__)
+
+
+setup(
+    name='simpleredisqueue',
+    version=__pkginfo__['version'],
+    description='Simple Redis queue',
+    url='https://github.com/cyber65535/',
+    author='Frank Jiao',
+    author_email="cyber65535@gmail.com",
+    keywords=['Redis', 'Queue'],
+    license='MIT',
+    install_requires=['redis'],
+    packages=['simpleredisqueue'],
+    classifiers=__pkginfo__['classifiers'],
+)
